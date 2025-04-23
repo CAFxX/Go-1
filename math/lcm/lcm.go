2667e3f34b894ea1e250fa397804f9aa83d00922
Carlo Alberto Ferraris <cafxx@strayorange.com>
2025-04-23 11:03:38 +0900

    lcm: avoid fp approximation

--- math/lcm/lcm.go
+++ math/lcm/lcm.go
@@ -1,12 +1,23 @@
 package lcm
 
 import (
-	"math"
+	"math/bits"
 
 	"github.com/TheAlgorithms/Go/math/gcd"
 )
 
 // Lcm returns the lcm of two numbers using the fact that lcm(a,b) * gcd(a,b) = | a * b |
 func Lcm(a, b int64) int64 {
-	return int64(math.Abs(float64(a*b)) / float64(gcd.Iterative(a, b)))
+	ua, ub := abs(a), abs(b)
+	h, l := bits.Mul64(ua, ub)
+	g := gcd.Iterative(a, b)
+	r := bits.Div64(h, l, uint64(g))
+	return int64(r)
+}
+
+func abs(x int64) uint64 {
+	if x < 0 {
+		return uint64(-(x+1))+1
+	}
+	return uint64(x)
 }
